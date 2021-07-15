8efbbab2ccc7fb0691e77f0a98f79c802fb8c60a
Avroneel <abhichy30@gmail.com>
2021-07-15 14:28:30 +0600

    Tic Tac Toe Game

---
+++ app/src/main/java/com/example/android/tictactoegame/GameDisplay.java
@@ -0,0 +1,46 @@
+package com.example.android.tictactoegame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class GameDisplay extends AppCompatActivity {
+    private TicTacToeBoard ticTacToeBoard;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_display);
+
+        Button playAgainBTN = findViewById(R.id.play_again);
+        Button homeBTN = findViewById(R.id.home_button);
+        TextView playerTurn = findViewById(R.id.player_display);
+
+        playAgainBTN.setVisibility(View.GONE);
+        playAgainBTN.setVisibility(View.GONE);
+
+        String [] playerNames = getIntent().getStringArrayExtra("PLAYER_NAMES");
+
+        if(playerNames != null){
+            playerTurn.setText((playerNames[0] + "'s Turn"));
+        }
+
+        ticTacToeBoard = findViewById(R.id.ticTacToeBoard);
+
+        ticTacToeBoard.setupGame(playAgainBTN,homeBTN,playerTurn,playerNames);
+
+    }
+    public void playAgainButtonClick(View view){
+        ticTacToeBoard.resetGame();
+        ticTacToeBoard.invalidate();
+
+    }
+    public void homeButtonClick(View view){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+}
